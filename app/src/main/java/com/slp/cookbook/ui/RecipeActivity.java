bfc2fe2bfcb65333881d8ba9c7f849ff4ba29e89
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-09-07 23:55:26 +0530

    feat: storing the recent recipe in SharedPreference using Gson

--- app/src/main/java/com/slp/cookbook/ui/RecipeActivity.java
+++ app/src/main/java/com/slp/cookbook/ui/RecipeActivity.java
@@ -1,11 +1,13 @@
 package com.slp.cookbook.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.gson.Gson;
 import com.slp.cookbook.R;
 import com.slp.cookbook.data.Recipe;
 import com.slp.cookbook.data.Steps;
@@ -26,6 +28,14 @@ public class RecipeActivity extends AppCompatActivity implements CookBookConstan
         RecipeStepsFragment recipeStepsFragment = new RecipeStepsFragment();
         recipeStepsFragment.setRecipeSteps(recipe.getSteps());
         getSupportFragmentManager().beginTransaction().add(R.id.recipe_steps, recipeStepsFragment).commit();
+        updateSharedPreferences();
+    }
+
+    private void updateSharedPreferences() {
+        Gson gson = new Gson();
+        SharedPreferences.Editor prefEditor = getSharedPreferences(COOKBOOK, MODE_PRIVATE).edit();
+        prefEditor.putString(RECIPE, gson.toJson(recipe));
+        prefEditor.commit();
     }
 
     public void showIngredients() {
@@ -34,9 +44,9 @@ public class RecipeActivity extends AppCompatActivity implements CookBookConstan
 
     @Override
     public void onClick(int position) {
-        Intent intent = new Intent(this,StepDetailActivity.class);
+        Intent intent = new Intent(this, StepDetailActivity.class);
         intent.putParcelableArrayListExtra(STEPS, (ArrayList<? extends Parcelable>) recipe.getSteps());
-        intent.putExtra(POSITION,position);
+        intent.putExtra(POSITION, position);
         startActivity(intent);
     }
 }
